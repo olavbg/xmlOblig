3832440e4a2f29da1ef43b739efe96230f4f9307
olavbg <olgulbra@hotmail.com>
2013-10-16 10:33:44 +0200

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    //include("include/db.php");
+    echo "TEST fra PHP";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Privat Regnskap</title>
+    </head>
+    <body>
+        <h1>Dette er en test</h1>
+        <p>For å se hvordan WebMatrix 3 funker!</p>
+    </body>
+</html>
+
